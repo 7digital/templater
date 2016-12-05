05ab23d4b564ba483b57de42722454127b38dcaa
Greg Sochanik <greg.sochanik@7digital.com>
2016-12-05 11:11:52 +0000

    Add method and test for replacement of tokens based on environment variables

--- src/Templater.Tests/EnvironmentVariableReplacementTests.cs
+++ src/Templater.Tests/EnvironmentVariableReplacementTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -8,20 +9,50 @@ namespace Templater.Tests
 	[TestFixture]
 	public class EnvironmentVariableReplacementTests
 	{
+		private const string INPUT_TEXT = "Example [%SomeKey%] \r\n" +
+			"Another example [%AnotherKey%] \r\n" +
+			"Another example [%AnUnmatchedKey%] \r\n" +
+			"You can't match this [%NoMatch";
+
 		[Test]
 		public void It_can_get_list_of_tokens_from_input_text()
 		{
-			var inputText = "Example [%SomeKey%] \r\n" +
-				"Another example [%AnotherKey%] \r\n" +
-				"Another example [%AnUnmatchedKey%] \r\n" +
-				"You can't match this [%NoMatch";
-
-			var actual = GetTokens(inputText).ToArray();
+			var actual = GetTokens(INPUT_TEXT).ToArray();
 
 			Assert.That(actual.Length, Is.EqualTo(3));
 			Assert.That(actual[0], Is.EqualTo("SomeKey"));
 			Assert.That(actual[1], Is.EqualTo("AnotherKey"));
 			Assert.That(actual[2], Is.EqualTo("AnUnmatchedKey"));
+		}
+
+		[Test]
+		public void It_can_swap_out_enviroment_variables_based_on_list_of_tokens()
+		{
+			System.Environment.SetEnvironmentVariable("SomeKey", "I replaced SomeKey");
+			System.Environment.SetEnvironmentVariable("AnotherKey", "I replaced AnotherKey");
+
+			var actual = SwapEnvironmentVariables(INPUT_TEXT);
+
+			var expected = "Example I replaced SomeKey \r\n" +
+				"Another example I replaced AnotherKey \r\n" +
+				"Another example [%AnUnmatchedKey%] \r\n" +
+				"You can't match this [%NoMatch";
+
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+
+		private object SwapEnvironmentVariables(string input)
+		{
+			foreach (var token in GetTokens(input))
+			{
+				var environmentVariable = System.Environment.GetEnvironmentVariable(token);
+				if (!string.IsNullOrEmpty(environmentVariable))
+				{
+					input = input.ReplaceKey(token, environmentVariable);
+				}
+			}
+
+			return input;
 		}
 
 		private IEnumerable<string> GetTokens(string input)
