b254804639ad16a15c2ad0ec25f36df6b7ec2b41
Greg Sochanik <greg.sochanik@7digital.com>
2016-12-06 16:27:27 +0000

    Add extra -e --environment parameter to console args

---
+++ src/Templater.Tests/ConsoleOptionsTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using CommandLine;
+
+namespace Templater.Tests
+{
+	public class ConsoleOptionsTests
+	{
+		private Options _options;
+
+		[SetUp]
+		public void Given_all_args_specified()
+		{
+			_options = new Options();
+			var args = new[] {"-d", "src", "-g", "settings.json", "-e", "local"};
+			Parser.Default.ParseArguments(args, _options);
+		}
+
+		[Test]
+		public void the_help_text_is_valid()
+		{
+			var helptext = _options.Help();
+			Console.WriteLine(helptext);
+			Assert.That(helptext, Is.StringContaining("-d, --directory"));
+			Assert.That(helptext, Is.StringContaining("Required. The working directory to recursively scan"));
+			Assert.That(helptext, Is.StringContaining("-g, --global-settings"));
+			Assert.That(helptext, Is.StringContaining("Path to a global settings json file"));
+			Assert.That(helptext, Is.StringContaining("-e, --environment"));
+			Assert.That(helptext, Is.StringContaining("(Default: all) Run for a specific environment only"));
+		}
+
+		[Test]
+		public void the_directory_is_specified()
+		{
+			Assert.That(_options.Directory, Is.EqualTo("src"));
+		}
+
+		[Test]
+		public void the_globalsettings_param_is_specified()
+		{
+			Assert.That(_options.GlobalSettingsPath, Is.EqualTo("settings.json"));
+		}
+
+		[Test]
+		public void the_environment_param_is_specified()
+		{
+			Assert.That(_options.RunEnvironment, Is.EqualTo("local"));
+		}
+	}
+}
--- src/Templater/Options.cs
+++ src/Templater/Options.cs
@@ -11,6 +11,9 @@ namespace Templater
 		[Option('g', "global-settings", Required = false, HelpText = "Path to a global settings json file")]
 		public string GlobalSettingsPath { get; set; }
 
+		[Option('e', "environment", Required = false, HelpText = "Run for a specific environment only - defaults to all", DefaultValue="all")]
+		public string RunEnvironment { get; set; }
+
 		[HelpOption]
 		public string Help()
 		{
