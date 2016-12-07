c0abb5066103954fb1d2fcc767bd0131cf9e3336
Greg Sochanik <greg.sochanik@7digital.com>
2016-12-07 10:31:53 +0000

    Remove unused and duplicated test

--- src/Templater.Tests/SettingsWriterTests.cs
+++ src/Templater.Tests/SettingsWriterTests.cs
@@ -36,35 +36,5 @@ namespace Templater.Tests
 			files.AssertWasCalled(x => x.WriteAllText(@"C:\web.uat.config", null));
 			files.AssertWasCalled(x => x.WriteAllText(@"C:\web.prod.config", null));
 		}
-
-		[Test]
-		public void It_writes_for_specific_environment_if_specified()
-		{
-			var files = MockRepository.GenerateStub<IFileWrapper>();
-			var replacer = MockRepository.GenerateStub<ISettingsReplacer>();
-
-			var writer = new SettingsWriter(files, replacer);
-			var settings = new Settings
-				{
-					Environments = new List<Environment>
-						{
-							new Environment
-								{
-									Name = "uat",
-									FileName = "web.uat.config",
-								},
-							new Environment
-								{
-									Name = "prod",
-									FileName = "web.prod.config",
-								},
-						}
-				};
-
-			writer.Write(@"C:\web.template.config", new Settings(), settings);
-
-			files.AssertWasCalled(x => x.WriteAllText(@"C:\web.uat.config", null));
-			files.AssertWasCalled(x => x.WriteAllText(@"C:\web.prod.config", null));
-		}
 	}
 }
